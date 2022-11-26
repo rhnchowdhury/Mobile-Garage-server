e5442ee6099e54512cf04fbf56adc1ffca6cba92
Raihan <rhnchdri@gmail.com>
2022-11-26 23:16:36 +0600

    server create

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middle ware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    res.send('mobile garage server running')
+});
+
+app.listen(port, () => console.log(`Mobile garage running on ${port}`))
