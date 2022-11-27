7956217822ce4c7cca54789c7fafaab9d9fcbab1
Raihan <rhnchdri@gmail.com>
2022-11-27 13:49:07 +0600

    limited catagories data created

--- index.js
+++ index.js
@@ -17,19 +17,41 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const iPhoneCollection = client.db('mobileGarage').collection('iPhone');
+        const onePlusCollection = client.db('mobileGarage').collection('onePlus');
+        const xiaomiCollection = client.db('mobileGarage').collection('xiaomi');
 
+        // iphone data create
         app.get('/iphone', async (req, res) => {
             const query = {};
             // const cursor = iPhoneCollection.find(query).limit(2)
             // const options = await cursor.toArray();
-            const options = await iPhoneCollection.find(query).limit(2).toArray();
-            res.send(options);
-        })
+            const options1 = await iPhoneCollection.find(query).limit(2).toArray();
+            res.send(options1);
+        });
         // app.get('/iphone', async (req, res) => {
         //     const query = {};
         //     const options = await iPhoneCollection.find(query).toArray();
         //     res.send(options);
         // })
+
+        // onePlus data created
+        app.get('/plus', async (req, res) => {
+            const query = {};
+            // const cursor = onePlusCollection.find(query).limit(2)
+            // const options = await cursor.toArray();
+            const options2 = await onePlusCollection.find(query).limit(2).toArray();
+            res.send(options2);
+        });
+
+        // xiaomi data created
+        app.get('/xiaomi', async (req, res) => {
+            const query = {};
+            // const cursor = iPhoneCollection.find(query).limit(2)
+            // const options = await cursor.toArray();
+            const options3 = await xiaomiCollection.find(query).limit(2).toArray();
+            res.send(options3);
+        });
+
     }
     finally {
 
