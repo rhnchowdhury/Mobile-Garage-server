aeb40f8a8541cf401642911323c9a893c06146f1
Raihan <rhnchdri@gmail.com>
2022-11-30 22:58:20 +0600

    all added

--- index.js
+++ index.js
@@ -106,6 +106,14 @@ async function run() {
             res.send(result);
         });
 
+        // admin check
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email };
+            const user = await usersCollection.findOne(query);
+            res.send({ isAdmin: user?.role === 'admin' });
+        });
+
         // jwt token create
         app.get('/jwt', async (req, res) => {
             const email = req.query.email;
