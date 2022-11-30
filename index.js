fcae1d10260758942a3b9d4fffdb31bfa1c973c1
Raihan <rhnchdri@gmail.com>
2022-11-30 09:54:13 +0600

    jwt implemented

--- index.js
+++ index.js
@@ -15,6 +15,21 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send('unauthorized access')
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next()
+    })
+};
+
 async function run() {
     try {
         const phoneCollection = client.db('mobileGarage').collection('phones');
@@ -39,8 +54,12 @@ async function run() {
         });
 
         //get booking data using email
-        app.get('/booking', async (req, res) => {
+        app.get('/booking', verifyJWT, async (req, res) => {
             const email = req.query.email;
+            const decodedEmail = req.decoded.email;
+            if (email !== decodedEmail) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
             const query = { userEmail: email };
             const bookings = await bookingCollection.find(query).toArray();
             res.send(bookings)
